35908f4268518379d0f62115a910b26e9436e9bb
Hussein Al Bayati <hussein@albayati.com>
2020-12-31 10:53:35 +0400

    Add project files.

---
+++ Com.H.Data/MappingExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+
+
+namespace Com.H.Data
+{
+
+    public static class MappingExtensions
+    {
+        private static readonly DataMapper _mapper = new DataMapper();
+
+        public static (string Name, PropertyInfo Info)[] GetCachedProperties(this Type type)
+        {
+            return _mapper.GetCachedProperties(type);
+        }
+
+
+        public static T Map<T>(this object source)
+        {
+            return _mapper.Map<T>(source);
+        }
+
+        public static T Clone<T>(this T source)
+        {
+            return _mapper.Clone<T>(source);
+        }
+
+        public static IEnumerable<T> Map<T>(this IEnumerable<object> source)
+        {
+            if (source == null) return null;
+            return _mapper.Map<T>(source);
+        }
+
+        public static void FillWith(
+            this object destination,
+            object source,
+            bool skipNull = false
+            )
+        {
+            _mapper.FillWith(destination, source, skipNull);
+        }
+    }
+}
